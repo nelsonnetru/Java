04473eb23995644c1d22a5092626060070e6db87
Aleksandr Ivanov <3911394@gmail.com>
2023-03-13 14:06:49 +0300

    Homework_02 added

--- homework_02/src/Main.java
+++ homework_02/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Class_01.main(args);
         Class_02.main(args);
+        Class_03.main(args);
+        Class_04.main(args);
     }
 }
